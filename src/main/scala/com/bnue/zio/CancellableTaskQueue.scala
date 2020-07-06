24fbd62a6f190c0815e6b1f39d3670114981074e
igarkusha <i.garkusha@mylivn.com>
2020-07-06 16:32:14 +0200

    states cleanup

--- src/main/scala/com/bnue/zio/CancellableTaskQueue.scala
+++ src/main/scala/com/bnue/zio/CancellableTaskQueue.scala
@@ -4,6 +4,7 @@ import zio.{Queue => _, _}
 import zio.interop.catz._
 import cats.implicits._
 import zio.Exit.{Failure, Success}
+import zio.stream.{Sink, ZStream}
 
 import scala.annotation.tailrec
 
@@ -14,7 +15,7 @@ object CancellableTaskQueue {
   private case class Add[K, E, V](key: K, task: IO[E, V], completeHook: Option[CompleteHook[E, V]]) extends Msg[K, E, V]
   private case class Cancel[K, E, V](key: K, hook: Option[CancelHook[E, V]], label: Option[String]) extends Msg[K, E, V]
   private case class Join[K, E, V](key: K, hook: CancelHook[E, V])                                  extends Msg[K, E, V]
-  private case class Completed[K, E, V](id: Int, key: K)                                            extends Msg[K, E, V]
+  private case class Completed[K, E, V](id: Int, key: K, exit: Exit[E, V])                          extends Msg[K, E, V]
   private case class GetKeys[K, E, V](p: Promise[Nothing, Set[K]])                                  extends Msg[K, E, V]
 
   sealed trait TaskCancelStatus[+E, +V]
@@ -32,8 +33,9 @@ object CancellableTaskQueue {
   private case class State[K, E, V](
       taskStore: Map[Int, Add[K, E, V]],
       keyStore: Map[K, Int],
-      inProgress: Option[(Int, Fiber[Nothing, TaskStatus[E, V]])],
+      inProgress: Option[(Int, Fiber[Nothing, Exit[E, V]])],
       cancelInProgress: Option[(Int, Fiber[Nothing, TaskStatus[E, V]])],
+      inProgressCancelRequestedBy: Option[String],
       lastAdddedId: Int
   )
 
@@ -61,168 +63,9 @@ object CancellableTaskQueue {
     val getRegisteredTaskKeys: UIO[Set[K]] = Promise.make[Nothing, Set[K]].flatMap(p => q.offer(GetKeys(p)) *> p.await)
   }
 
-  def runProcessing[K, E, V](
-      q: Queue[Msg[K, E, V]],
-      shutdown: Ref[Boolean],
-      cancelRequestedBy: Ref[Option[String]],
-      taskStoreSize: Int,
-      st: State[K, E, V]
-  ): UIO[Unit] = {
-
-    def toStatus(e: Exit[E, V], cancelReq: Option[String]): TaskStatus[E, V] = e match {
-      case Success(v) => Done(v)
-      case Failure(cause) =>
-        if (cause.interrupted) Cancelled(cancelReq)
-        else if (cause.failed) Failed(cause.failures)
-        else Died(cause.defects)
-    }
-
-    def findId(startId: Int, f: Int => Boolean, msg: => String): Int = {
-      @tailrec
-      def find(currentId: Int): Int = {
-        val rolledId = currentId % taskStoreSize
-        if (f(rolledId)) rolledId
-        //  startId == rolledId means that we rolled over the whole ids space
-        else if (startId != rolledId) find(rolledId + 1)
-        else throw new AssertionError(msg)
-      }
-      find(startId + 1)
-    }
-
-    def runTask(id: Int, add: Add[K, E, V]): UIO[Fiber[Nothing, TaskStatus[E, V]]] =
-      IO.whenM(st.inProgress.fold(IO.succeed(false))(_._2.poll.map(_.isEmpty)))(
-          IO.dieMessage("trying to start new task when something is in progress")
-      ) *>
-        add.task
-        // whe whole execution block (message processing) where runTask is called is uninterruptible
-          .interruptible
-          .run
-          .flatMap(exit => cancelRequestedBy.modify(cr => toStatus(exit, cr) -> None))
-          // Completed is guaranteed to be added to queue before hook succeeds and user that waits for hook will only be able to observe the state as it is AFTER Completed is processed
-          .tap(status => q.offer(Completed(id, add.key)) *> add.completeHook.traverse_(_.succeed(status)))
-          .fork
-
-    q.take
-      .flatMap {
-        case add: Add[K, E, V] =>
-          if (st.keyStore.contains(add.key)) add.completeHook.traverse_(_.succeed(Duplicate)).as(st)
-          else if (st.taskStore.size == taskStoreSize) add.completeHook.traverse_(_.succeed(Rejected)).as(st)
-          else {
-            val id = findId(
-                st.lastAdddedId,
-                v => !st.taskStore.contains(v),
-                "cannot find vacant id in non-full taskStore"
-            )
-
-            st.inProgress
-              .orElseIO(runTask(id, add).map(t => Some(id -> t)))
-              .map(inProg => st.copy(st.taskStore.updated(id, add), st.keyStore.updated(add.key, id), inProg, lastAdddedId = id))
-          }
-
-        case Cancel(key, cancelHookOpt, label) =>
-          st.keyStore.get(key) match {
-            case None => cancelHookOpt.traverse_(_.succeed(NotFound)).as(st)
-            case Some(id) =>
-              st.cancelInProgress match {
-                // when cancelInprogress fiber is completed Completed msg is guaranteed to be in the queue user will always see the proper state
-                case Some(`id` -> fiber) => cancelHookOpt.traverse_(_.completeWith(fiber.join)).as(st)
-                case _ =>
-                  st.inProgress match {
-                    case Some(`id` -> fiber) =>
-                      cancelRequestedBy.set(label) *>
-                        fiber
-                          .interrupt
-                          .map {
-                            case Success(v) => v
-                            case e          => throw new AssertionError(s"failure during the interruption of sandboxed IO $e")
-                          }
-                          .fork
-                          .flatMap(
-                              interruptFiber =>
-                                cancelHookOpt
-                                  .traverse_(_.completeWith(interruptFiber.join))
-                                  .as(st.copy(cancelInProgress = Some(id -> interruptFiber)))
-                          )
-                    case _ =>
-                      unsafe(st.taskStore(id))("key-id is not in sync")
-                        .completeHook
-                        .traverse_(_.succeed(Cancelled(label)))
-                        .*>(cancelHookOpt.traverse_(_.succeed(Cancelled(label))))
-                        .as(st.copy(st.taskStore - id, st.keyStore - key))
-                  }
-              }
-          }
-        case Join(key, joinHook) =>
-          // Dirty hack to save a bit of runtime overhead: hooks are under control of this library and we '_know_' that Duplicate message cannot appear at this stage
-          // To do this in type safe manner we'd have to put 2 Promises into 'Add' msg: 1) TaskStatus, 2)Duplicate.type and race between them. This way we could ensure that duplicate msg never shows up in this method.
-          val awaitAndTrimForbiddenMsgs: CompleteHook[E, V] => UIO[TaskStatus[E, V]] =
-            _.await.flatMap {
-              case t @ (Duplicate | Rejected) => IO.dieMessage(s"in progress task is completed with '$t'")
-              case h: TaskStatus[E, V]        => IO.succeed(h)
-            }
-
-          st.keyStore.get(key) match {
-            case None => joinHook.succeed(NotFound).as(st)
-            case Some(id) =>
-              st.inProgress match {
-                case Some((`id`, fiber)) => joinHook.completeWith(fiber.join).as(st)
-                case _ =>
-                  val add = unsafe(st.taskStore(id))("key-id is not in sync")
-                  add.completeHook match {
-                    case Some(completeHook) => joinHook.completeWith(awaitAndTrimForbiddenMsgs(completeHook)).as(st)
-                    case None =>
-                      for {
-                        completeHook <- Promise.make[Nothing, TaskCompletionStatus[E, V]]
-                        _            <- joinHook.completeWith(awaitAndTrimForbiddenMsgs(completeHook))
-                      } yield st.copy(taskStore = st.taskStore.updated(id, add.copy(completeHook = Some(completeHook))))
-                  }
-              }
-          }
-
-        case GetKeys(p) => p.succeed(st.keyStore.keySet).as(st)
-
-        case Completed(id, key) =>
-          st.cancelInProgress.traverse_ {
-            case (cid, task) =>
-              assert(cid == id, s"Completed key '$key' does not match key of the task being cancelled '$cid'")
-              //guarantees that cancelHookOpt will be fulfilled before fiber gets garbage collected
-              task.join
-          } *>
-            ZIO
-              .optional(st.taskStore.size > 1) {
-                val nextId = findId(
-                    id,
-                    st.taskStore.contains,
-                    "cannot find next task in nonempty taskstore"
-                )
-                // Completed message is sent from the inProgress fiber after the task is completed. Waiting for this fiber to complete before running next task.
-                st.inProgress.traverse_(_._2.join) *>
-                  runTask(nextId, st.taskStore(nextId)).map(nextId -> _)
-              }
-              .map(inProg => st.copy(st.taskStore - id, st.keyStore - key, inProg, None))
-      }
-      .uninterruptible
-      .flatMap { st =>
-        // controlled shutdown: this code can be interrupted only before or after message extraction+processing
-        shutdown
-          .get
-          .flatMap(
-              sd =>
-                if (!sd) runProcessing(q, shutdown, cancelRequestedBy, taskStoreSize, st)
-                else {
-                  st.inProgress.traverse_(_._2.interrupt) *>
-                    //guarantees that cancelHookOpt will be fulfilled
-                    st.cancelInProgress.traverse_(_._2.join) *>
-                    UIO.foreach_(st.taskStore)(_._2.completeHook.traverse_(_.interrupt))
-                }
-          )
-      }
-  }
-
   def apply[K, E, V](commandQueueCapacity: Int = 1024, taskStoreCapacity: Int = Int.MaxValue): ZManaged[Any, Nothing, Ops[K, E, V]] =
     for {
-      shutdown          <- ZManaged.fromEffect(Ref.make(false))
-      cancelRequestedBy <- ZManaged.fromEffect(Ref.make[Option[String]](None))
+      shutdown <- ZManaged.fromEffect(Ref.make(false))
 
       commandQueue <- ZManaged.make(zio.Queue.bounded[Msg[K, E, V]](commandQueueCapacity).flatMap(Queue.wrap(_)))(
                          v =>
@@ -236,13 +79,13 @@ object CancellableTaskQueue {
                      )
 
       ops = new Ops[K, E, V](commandQueue)
+
       _ <- ZManaged.make(
               runProcessing[K, E, V](
                   commandQueue,
                   shutdown,
-                  cancelRequestedBy,
                   taskStoreCapacity,
-                  State(Map.empty, Map.empty, None, None, -1)
+                  State(Map.empty, Map.empty, None, None, None, -1)
               ).fork
           )(
               t =>
@@ -252,4 +95,156 @@ object CancellableTaskQueue {
                   *> t.join
           )
     } yield ops
+
+  private def runProcessing[K, E, V](
+      q: Queue[Msg[K, E, V]],
+      shutdown: Ref[Boolean],
+      taskStoreSize: Int,
+      currentState: State[K, E, V]
+  ): UIO[Unit] =
+    ZIO.ifM(shutdown.get)(
+        // controlled shutdown: this code can be interrupted only before or after message extraction+processing
+        currentState.inProgress.traverse_(_._2.interrupt) *>
+          //guarantees that cancelHookOpt will be fulfilled
+          currentState.cancelInProgress.traverse_(_._2.join) *>
+          UIO.foreach_(currentState.taskStore)(_._2.completeHook.traverse_(_.interrupt)),
+        for {
+          nextMsg   <- q.take
+          nextState <- runState(taskStoreSize, q, currentState, nextMsg)
+          _         <- runProcessing(q, shutdown, taskStoreSize, nextState)
+        } yield ()
+    )
+
+  private def runState[K, E, V](taskStoreSize: Int, q: Queue[Msg[K, E, V]], st: State[K, E, V], msg: Msg[K, E, V]): UIO[State[K, E, V]] = msg match {
+    case add: Add[K, E, V] =>
+      if (st.keyStore.contains(add.key)) add.completeHook.traverse_(_.succeed(Duplicate)).as(st)
+      else if (st.taskStore.size == taskStoreSize) add.completeHook.traverse_(_.succeed(Rejected)).as(st)
+      else {
+        val id = findId(
+            st.lastAdddedId,
+            v => !st.taskStore.contains(v),
+            taskStoreSize,
+            "cannot find vacant id in non-full taskStore"
+        )
+
+        st.inProgress
+          .orElseIO(runTask(id, add, st.inProgress.map(_._2), q).map(t => Some(id -> t)))
+          .map(inProg => st.copy(st.taskStore.updated(id, add), st.keyStore.updated(add.key, id), inProg, lastAdddedId = id))
+      }
+
+    case Cancel(key, cancelHookOpt, label) =>
+      st.keyStore.get(key) match {
+        case None => cancelHookOpt.traverse_(_.succeed(NotFound)).as(st)
+        case Some(id) =>
+          st.cancelInProgress match {
+            // when cancelInprogress fiber is completed Completed msg is guaranteed to be in the queue user will always see the proper state
+            case Some(`id` -> fiber) => cancelHookOpt.traverse_(_.completeWith(fiber.join)).as(st)
+            case _ =>
+              st.inProgress match {
+                case Some(`id` -> fiber) =>
+                  fiber
+                    .interrupt
+                    .map {
+                      case Success(v) => v
+                      case e          => throw new AssertionError(s"failure during the interruption of sandboxed IO $e")
+                    }
+                    .fork
+                    .flatMap { interruptFiber =>
+                      val interruptFiberWithStatus = interruptFiber.map(toStatus(_, cancelRequestedBy = label))
+                      cancelHookOpt
+                        .traverse_(_.completeWith(interruptFiberWithStatus.join))
+                        .as(st.copy(cancelInProgress = Some(id -> interruptFiberWithStatus), inProgressCancelRequestedBy = label))
+                    }
+                case _ =>
+                  unsafe(st.taskStore(id))("key-id is not in sync")
+                    .completeHook
+                    .traverse_(_.succeed(Cancelled(label)))
+                    .*>(cancelHookOpt.traverse_(_.succeed(Cancelled(label))))
+                    .as(st.copy(st.taskStore - id, st.keyStore - key))
+              }
+          }
+      }
+    case Join(key, joinHook) =>
+      // Dirty hack to save a bit of runtime overhead: hooks are under control of this library and we '_know_' that Duplicate and Reject messages cannot appear at this stage
+      val awaitAndTrimForbiddenMsgs: CompleteHook[E, V] => UIO[TaskStatus[E, V]] =
+        _.await.flatMap {
+          case t @ (Duplicate | Rejected) => IO.dieMessage(s"in progress task is completed with '$t'")
+          case h: TaskStatus[E, V]        => IO.succeed(h)
+        }
+
+      st.keyStore.get(key) match {
+        case None => joinHook.succeed(NotFound).as(st)
+        case Some(id) =>
+          val add = unsafe(st.taskStore(id))("key-id is not in sync")
+          add.completeHook match {
+            case Some(completeHook) => joinHook.completeWith(awaitAndTrimForbiddenMsgs(completeHook)).as(st)
+            case None =>
+              for {
+                completeHook <- Promise.make[Nothing, TaskCompletionStatus[E, V]]
+                _            <- joinHook.completeWith(awaitAndTrimForbiddenMsgs(completeHook))
+              } yield st.copy(taskStore = st.taskStore.updated(id, add.copy(completeHook = Some(completeHook))))
+          }
+      }
+
+    case GetKeys(p) => p.succeed(st.keyStore.keySet).as(st)
+
+    case Completed(id, key, exit) =>
+      st.cancelInProgress.traverse_ {
+        case (cid, taskFiber) =>
+          assert(cid == id, s"Completed key '$key' does not match key of the task being cancelled '$cid'")
+          //guarantees that cancelHookOpt will be fulfilled before fiber gets garbage collected
+          taskFiber.join
+      } *>
+        // Completed message is sent from the inProgress fiber after the task is completed. This fiber should be already completed
+        st.inProgress.traverse_(_._2.join) *>
+        unsafe(st.taskStore(id))("key-id is not in sync").completeHook.traverse_(_.succeed(toStatus(exit, st.inProgressCancelRequestedBy))) *>
+        ZIO
+          .optional(st.taskStore.size > 1) {
+            val nextId = findId(
+                id,
+                st.taskStore.contains,
+                taskStoreSize,
+                "cannot find next task in nonempty taskstore"
+            )
+            runTask(nextId, st.taskStore(nextId), st.inProgress.map(_._2), q).map(nextId -> _)
+          }
+          .map(inProg => st.copy(st.taskStore - id, st.keyStore - key, inProg, None, None))
+  }
+
+  private def toStatus[E, V](e: Exit[E, V], cancelRequestedBy: Option[String]): TaskStatus[E, V] = e match {
+    case Success(v) => Done(v)
+    case Failure(cause) =>
+      if (cause.interrupted) Cancelled(cancelRequestedBy)
+      else if (cause.failed) Failed(cause.failures)
+      else Died(cause.defects)
+  }
+
+  private def findId(startId: Int, f: Int => Boolean, taskStoreSize: Int, msg: => String): Int = {
+    @tailrec
+    def find(currentId: Int): Int = {
+      val rolledId = currentId % taskStoreSize
+      if (f(rolledId)) rolledId
+      //  startId == rolledId means that we rolled over the whole ids space
+      else if (startId != rolledId) find(rolledId + 1)
+      else throw new AssertionError(msg)
+    }
+    find(startId + 1)
+  }
+
+  private def runTask[K, E, V](
+      id: Int,
+      add: Add[K, E, V],
+      inProgress: Option[Fiber[Nothing, Exit[E, V]]],
+      q: Queue[Msg[K, E, V]]
+  ): UIO[Fiber[Nothing, Exit[E, V]]] =
+    IO.whenM(inProgress.fold(IO.succeed(false))(_.poll.map(_.isEmpty)))(
+        IO.dieMessage("trying to start new task when something is in progress")
+    ) *>
+      add.task
+      // making task itself interruptible while all surrounding is uninterruptible
+        .interruptible
+        .run
+        .tap(exit => q.offer(Completed(id, add.key, exit)))
+        .fork
+
 }
--- src/main/scala/com/bnue/zio/Queue.scala
+++ src/main/scala/com/bnue/zio/Queue.scala
@@ -10,28 +10,28 @@ object Queue {
 }
 
 // ordinary ZIO queue with additional 'shutdownAndTakeAll' method.
-class Queue[A](q: zio.Queue[A], s: Semaphore, numberOfPermits: Int, shutdownFlag: Ref[Boolean]) extends ZQueue[Any, Nothing, Any, Nothing, A, A] {
+class Queue[A](val underlying: zio.Queue[A], s: Semaphore, numberOfPermits: Int, shutdownFlag: Ref[Boolean]) extends ZQueue[Any, Any, Nothing, Nothing, A, A] {
   private def withPermit[T](f: UIO[T]): UIO[T] =
     cond(shutdownFlag.get)(UIO.interrupt, s.withPermit(f))
-  override def take: ZIO[Any, Nothing, A] = withPermit(q.take)
+  override def take: ZIO[Any, Nothing, A] = withPermit(underlying.take)
 
-  override def awaitShutdown: UIO[Unit] = withPermit(q.awaitShutdown)
+  override def awaitShutdown: UIO[Unit] = withPermit(underlying.awaitShutdown)
 
-  override def capacity: Int = q.capacity
+  override def capacity: Int = underlying.capacity
 
-  override def isShutdown: UIO[Boolean] = withPermit(q.isShutdown)
+  override def isShutdown: UIO[Boolean] = withPermit(underlying.isShutdown)
 
-  override def offer(a: A): UIO[Boolean] = withPermit(q.offer(a))
+  override def offer(a: A): UIO[Boolean] = withPermit(underlying.offer(a))
 
-  override def offerAll(as: Iterable[A]): UIO[Boolean] = withPermit(q.offerAll(as))
+  override def offerAll(as: Iterable[A]): UIO[Boolean] = withPermit(underlying.offerAll(as))
 
-  override def size: UIO[Int] = withPermit(q.size)
+  override def size: UIO[Int] = withPermit(underlying.size)
 
-  override def takeAll: UIO[List[A]] = withPermit(q.takeAll)
+  override def takeAll: UIO[List[A]] = withPermit(underlying.takeAll)
 
-  override def takeUpTo(max: Int): UIO[List[A]] = withPermit(q.takeUpTo(max))
+  override def takeUpTo(max: Int): UIO[List[A]] = withPermit(underlying.takeUpTo(max))
 
-  override def shutdown: UIO[Unit] = withPermit(q.shutdown)
+  override def shutdown: UIO[Unit] = withPermit(underlying.shutdown)
 
-  def shutdownAndTakeAll: UIO[List[A]] = shutdownFlag.set(true) *> s.withPermits(numberOfPermits)(q.takeAll)
+  def shutdownAndTakeAll: UIO[List[A]] = shutdownFlag.set(true) *> s.withPermits(numberOfPermits)(underlying.takeAll)
 }
